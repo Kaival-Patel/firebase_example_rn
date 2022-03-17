f35ff98b1d9fa09bfea3a2a3784e27e9f3018c8c
Kaival Patel <39383435+Kaival-Patel@users.noreply.github.com>
2022-03-17 09:25:58 +0530

    Login Screen State Ready

--- src/components/text_input.js
+++ src/components/text_input.js
@@ -2,64 +2,85 @@ import React from 'react';
 import {useState, useContext} from 'react';
 import {TextInput, StyleSheet} from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import {getColor, useTheme, View, TextField, Input, Icon, Center} from 'native-base';
+import {
+  getColor,
+  useTheme,
+  View,
+  TextField,
+  Input,
+  Icon,
+  Center,
+} from 'native-base';
 export const CustomTextInput = ({
   label,
   icon,
   error,
   password,
   onFocus = () => {},
+  onChangeText = (value) => {},
 }) => {
   const [hidePassword, setHidePassword] = useState(true);
   const [isFocused, setIsFocused] = useState(false);
-  const {colors} = useTheme();
   return (
     <Center>
       <Input
-      w={{
-        base: '90%',
-        md:'90%',
-        
-      }}
-      InputLeftElement={
-        <Icon
-          as={
-            <FontAwesome
-              style={{
-                ...style.icon,
-                color: isFocused ? 'brand.200' : '#C0C0C0',
-              }}
-              name={icon}
+        w={{
+          base: '90%',
+          md: '90%',
+        }}
+        InputLeftElement={
+          <Icon
+            as={
+              <FontAwesome
+                style={{
+                  ...style.icon,
+                  color: isFocused ? 'brand.200' : '#C0C0C0',
+                }}
+                name={icon}
+              />
+            }
+            size={10}
+            ml="2"
+            color="primary.200"
+          />
+        }
+        InputRightElement={
+          password ? (
+            <Icon
+              as={
+                <FontAwesome
+                  style={{
+                    ...style.icon,
+                    color: isFocused ? 'brand.200' : '#C0C0C0',
+                  }}
+                  name={hidePassword ? 'eye-slash' : 'eye'}
+                />
+              }
+              size={10}
+              onPress={() => setHidePassword(!hidePassword)}
             />
-          }
-          size={10}
-          ml="2"
-          color="primary.200"
-        />
-      }
-      onFocus={() => {
-        onFocus();
-        setIsFocused(true);
-      }}
-      onBlur={() => {
-        setIsFocused(false);
-      }}
-      borderColor={isFocused ? 'primary.100' : '#C0C0C0'}
-      variant="outline"
-      placeholder={label}
-      fontSize={16}
-    />
+          ) : null
+        }
+        onFocus={() => {
+          onFocus();
+          setIsFocused(true);
+        }}
+        onChangeText={onChangeText}
+        type={password ? (hidePassword ? 'password' : 'text') : 'default'}
+        onBlur={() => {
+          setIsFocused(false);
+        }}
+        passwordRules="required: upper; required: lower; required: digit; max-consecutive: 2; minlength: 8"
+        borderColor={isFocused ? 'primary.100' : '#C0C0C0'}
+        variant="outline"
+        placeholder={label}
+        fontSize={16}
+      />
     </Center>
   );
 };
 
 const style = StyleSheet.create({
-  container: {
-    marginHorizontal: 10,
-    marginVertical: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   icon: {
     marginHorizontal: 10,
     fontSize: 20,
--- src/screens/login_screen.js
+++ src/screens/login_screen.js
@@ -1,3 +1,4 @@
+import {Button, FormControl} from 'native-base';
 import React from 'react';
 import {
   View,
@@ -8,15 +9,96 @@ import {
   TextInput,
 } from 'react-native';
 import {LOGIN_COVER} from '../assets/images/index';
-import { CustomTextInput } from '../components/text_input';
+import {CustomTextInput} from '../components/text_input';
 import Fonts from '../global/fonts';
 export const LoginScreen = ({navigation, route}) => {
+  const [invalidPassword, setInValidPassword] = React.useState(false);
+  const [formErrorData, setFormErrorData] = React.useState({
+    email: '',
+    password: '',
+  });
+  const [invalidEmail, setInvalidEmail] = React.useState(false);
+  const [formData, setData] = React.useState({});
+  console.log(formErrorData);
+  const handleLogin = () => {
+    if (formData.email == undefined || formData.email == '') {
+      // setFormErrorData({...formErrorData, email: 'Email is required'});
+      formErrorData = {...formErrorData, email: 'Email is required'};
+      setInvalidEmail(true);
+    } else {
+      let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+      if (reg.test(formData.email) === false) {
+        formErrorData = {...formErrorData, email: 'Please enter a valid email'};
+        // setFormErrorData({
+        //   ...formErrorData,
+        //   email: 'Please enter a valid email',
+        // });
+        setInvalidEmail(true);
+      } else {
+        setInvalidEmail(false);
+      }
+    }
+    if (formData.password == undefined || formData.password == '') {
+      console.info(formErrorData);
+      formErrorData={...formErrorData, password: 'Please enter password'};
+      // setFormErrorData({...formErrorData, password: 'Please enter password'});
+      setInValidPassword(true);
+    } else if (formData.password.length < 6) {
+      formErrorData = {
+        ...formErrorData,
+        password: 'Please provide password with minimum 6 characters',
+      };
+      // setFormErrorData({
+      //   ...formErrorData,
+      //   password: 'Please provide password with minimum 6 characters',
+      // });
+      setInValidPassword(true);
+    }
+    setFormErrorData(formErrorData);
+    if(!invalidEmail && !invalidPassword){
+      
+    }
+  };
   return (
     <ScrollView style={styles.scaffold}>
       <View>
         <Image source={LOGIN_COVER} style={styles.login_cover} />
         <Text style={styles.header}>Login</Text>
-        <CustomTextInput icon="at" label={"Email"} onFocus={()=>{}}  />
+        <FormControl isInvalid={invalidEmail}>
+          <CustomTextInput
+            onChangeText={value => {
+              setData({...formData, email: value});
+            }}
+            icon="at"
+            label={'Email'}
+            onFocus={() => {}}
+          />
+          <FormControl.ErrorMessage
+            marginLeft={5}
+            w={{
+              base: '90%',
+              md: '90%',
+            }}>
+            {formErrorData.email}
+          </FormControl.ErrorMessage>
+        </FormControl>
+        <FormControl isInvalid={invalidPassword} marginTop={5}>
+          <CustomTextInput
+            label={'Password'}
+            onFocus={() => {}}
+            icon="lock"
+            onChangeText={value => {
+              setData({...formData, password: value});
+            }}
+            password={true}
+          />
+          <FormControl.ErrorMessage marginLeft={5}>
+            {formErrorData.password}
+          </FormControl.ErrorMessage>
+        </FormControl>
+        <Button margin={5} bgColor="primary.100" onPress={handleLogin}>
+          Login
+        </Button>
       </View>
     </ScrollView>
   );
@@ -42,10 +124,9 @@ const styles = StyleSheet.create({
   textfield: {
     marginHorizontal: 10,
     marginVertical: 10,
-    borderColor:"#C0C0C0",
-    borderWidth:1,
-    borderRadius:10,
-    padding:10,
-    
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
   },
 });
