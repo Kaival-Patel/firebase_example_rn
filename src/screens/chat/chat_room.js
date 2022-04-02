5d58a5f023b1490abdfef8253b818cb3fbb82cfb
Kaival Patel <39383435+Kaival-Patel@users.noreply.github.com>
2022-04-02 09:07:58 +0530

    Search done

---
+++ src/screens/chat/chat_room.js
@@ -0,0 +1,1 @@
+export const ChatRoom = ({navigation, route, chatRoomId}) => {};
--- src/screens/chat/new_chat_search.js
+++ src/screens/chat/new_chat_search.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {Center, FlatList, Spinner, Text, View} from 'native-base';
+import {FlatList} from 'react-native';
+import {Avatar, Center, Spinner, Text, View} from 'native-base';
 import {SearchBar} from 'react-native-screens';
 import {CustomTextInput} from '../../components/text_input';
 import {getAllUser} from '../../backend/user_service';
 import {storyData} from '../../constants/constant';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
-export const NewChatSearch = () => {
+export const NewChatSearch = ({navigation, route}) => {
   const [search, setSearch] = React.useState('');
   const [loading, setIsLoading] = React.useState(true);
   const [searchedResult, setSearchedResult] = React.useState([]);
@@ -15,30 +17,43 @@ export const NewChatSearch = () => {
       setIsLoading(false);
       setDbData([]);
       res.docs.forEach(doc => {
-        console.log(doc.data());
         dbData.push(doc.data());
         setDbData(dbData);
       });
     });
   }, []);
-
-  React.useEffect(() => {
+  function handleSearch() {
+    setSearchedResult([]);
     dbData.forEach(doc => {
       if (doc.name.toLowerCase().includes(search.toLowerCase())) {
-        console.info(doc.name);
-        searchedResult.push(doc);
-        setSearchedResult(searchedResult);
+        if (!searchedResult.includes(doc)) {
+          searchedResult.push(doc);
+          setSearchedResult(searchedResult);
+        }
       }
-      console.log(searchedResult.length);
     });
-  }, [search]);
+    console.log(searchedResult);
+  }
+  // React.useEffect(() => {
+  //   setSearchedResult([]);
+  //   dbData.forEach(doc => {
+  //     if (doc.name.toLowerCase().includes(search.toLowerCase())) {
+  //       if (!searchedResult.includes(doc)) {
+  //         searchedResult.push(doc);
+  //         setSearchedResult(searchedResult);
+  //       }
+  //     }
+  //   });
+  //   console.log(searchedResult);
+  // }, [search]);
   return (
-    <View style={{flex: 1}}>
+    <View>
       <View marginY={5}>
         <CustomTextInput
           onChangeText={value => {
             setSearchedResult([]);
             setSearch(value);
+            handleSearch();
           }}
           icon="search"
           label={'Search for people using name'}
@@ -49,15 +64,35 @@ export const NewChatSearch = () => {
         <Center>
           <Spinner color="primary.100" />
         </Center>
-      ) : null}
-      <FlatList
-        data={storyData}
-        renderItem={({item}) => {
-          <View height={50}>
-            <Text>{item.name}</Text>
-          </View>;
-        }}
-      />
+      ) : (
+        <FlatList
+          data={search === '' ? dbData : searchedResult}
+          renderItem={({item}) => (
+            <TouchableOpacity onPress={() => {}}>
+              <View
+                flexDirection={'row'}
+                alignItems="center"
+                height="65px"
+                marginX={5}>
+                <Avatar
+                  bg="primary.50"
+                  marginX="10px"
+                  marginY="5px"
+                  size="45px"
+                  source={{uri: item.photo}}
+                />
+                <View>
+                  <Text fontSize={'17px'}>{item.name}</Text>
+                  <Text fontSize={'12px'} color="gray.400">
+                    {item.email}
+                  </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      )}
+      <View height={100}></View>
     </View>
   );
 };
